862edeee7e25f442993edef8dc10b9ac343b482c
HP <basirkhan.hashemi@gmail.com>
2026-02-25 00:34:02 +0430

    invoice commit

--- lib/Views/Menu/Ui/Projects/Ui/ProjectServices/project_services.dart
+++ lib/Views/Menu/Ui/Projects/Ui/ProjectServices/project_services.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:zaitoon_petroleum/Features/Other/cover.dart';
 import 'package:zaitoon_petroleum/Features/Other/extensions.dart';
 import 'package:zaitoon_petroleum/Features/Other/responsive.dart';
 import 'package:zaitoon_petroleum/Features/Widgets/no_data_widget.dart';
+import 'package:zaitoon_petroleum/Features/Widgets/outline_button.dart';
 import 'package:zaitoon_petroleum/Features/Widgets/textfield_entitled.dart';
 import 'package:zaitoon_petroleum/Localizations/Bloc/localizations_bloc.dart';
 import 'package:zaitoon_petroleum/Localizations/l10n/translations/app_localizations.dart';
 import 'package:zaitoon_petroleum/Views/Menu/Ui/Projects/Ui/ProjectServices/bloc/project_services_bloc.dart';
+import 'package:zaitoon_petroleum/Views/Menu/Ui/Projects/Ui/ProjectServices/model/project_services_model.dart';
 import 'package:zaitoon_petroleum/Views/Menu/Ui/Settings/Ui/Services/Ui/add_edit_services.dart';
 import 'package:zaitoon_petroleum/Views/Menu/Ui/Settings/Ui/Services/bloc/services_bloc.dart';
 import 'package:zaitoon_petroleum/Views/Menu/Ui/Settings/Ui/Services/model/services_model.dart';
 
 import '../../../../../../Features/Generic/rounded_searchable_textfield.dart';
+import '../../../../../Auth/bloc/auth_bloc.dart';
+import '../../../../../Auth/models/login_model.dart';
 
 class ProjectServicesView extends StatelessWidget {
   final int? projectId;
@@ -55,9 +60,10 @@ class _Desktop extends StatefulWidget {
 class _DesktopState extends State<_Desktop> {
   final formKey = GlobalKey<FormState>();
   final servicesController = TextEditingController();
-  final qty = TextEditingController();
+  final qty = TextEditingController(text: "1");
   final amount = TextEditingController();
   final remark = TextEditingController();
+  int? serviceId;
   String? myLocale;
   @override
   void initState() {
@@ -69,7 +75,7 @@ class _DesktopState extends State<_Desktop> {
     });
     super.initState();
   }
-
+  LoginData? loginData;
   @override
   void dispose() {
     remark.dispose();
@@ -85,93 +91,98 @@ class _DesktopState extends State<_Desktop> {
     final color = Theme.of(context).colorScheme;
     TextStyle? titleStyle = Theme.of(context).textTheme.titleSmall;
     TextStyle? subtitleStyle = Theme.of(context).textTheme.titleSmall?.copyWith(color: color.outline.withValues(alpha: .8));
+
+    final state = context.watch<AuthBloc>().state;
+    if (state is! AuthenticatedState) {
+      return const SizedBox();
+    }
+    loginData = state.loginData;
+
     return Scaffold(
       body: Column(
         children: [
-          Padding(
+          ZCover(
             padding: const EdgeInsets.all(8.0),
             child: Column(
               children: [
 
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 2, vertical: 5),
-                  child:
-                  GenericTextfield<ServicesModel, ServicesBloc, ServicesState>(
-                    showAllOnFocus: true,
-                    controller: servicesController,
-                    title: tr.services,
-                    hintText: tr.services,
-                    trailing: IconButton(
-                      onPressed: () {
-                        showDialog(
-                          context: context,
-                          builder: (context) {
-                            return AddEditServiceView();
-                          },
-                        );
-                      },
-                      icon: Icon(Icons.add),
-                    ),
-                    isRequired: true,
-                    bloc: context.read<ServicesBloc>(),
-                    fetchAllFunction: (bloc) => bloc.add(LoadServicesEvent()),
-                    searchFunction: (bloc, query) => bloc.add(LoadServicesEvent(search: query)),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return tr.required(tr.individuals);
-                      }
-                      return null;
-                    },
-                    itemBuilder: (context, ser) => Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 5,
-                        vertical: 8,
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Expanded(
-                                child: Text(
-                                  "${ser.srvName}",
-                                  style: Theme.of(context).textTheme.bodyLarge,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    itemToString: (ser) => "${ser.srvName}",
-                    stateToLoading: (state) => state is ServicesLoadingState,
-                    loadingBuilder: (context) => const SizedBox(
-                      width: 16,
-                      height: 16,
-                      child: CircularProgressIndicator(strokeWidth: 2),
-                    ),
-                    stateToItems: (state) {
-                      if (state is ServicesLoadedState) {
-                        return state.services;
-                      }
-                      return [];
-                    },
-                    onSelected: (value) {
-                      setState(() {
-
-                      });
-                    },
-                    noResultsText: tr.noDataFound,
-                    showClearButton: true,
-                  ),
-                ),
                Row(
                  spacing: 8,
                  children: [
-
+                   Expanded(
+                     flex: 3,
+                     child: GenericTextfield<ServicesModel, ServicesBloc, ServicesState>(
+                       controller: servicesController,
+                       title: tr.services,
+                       hintText: tr.services,
+                       trailing: IconButton(
+                         onPressed: () {
+                           showDialog(
+                             context: context,
+                             builder: (context) {
+                               return AddEditServiceView();
+                             },
+                           );
+                         },
+                         icon: Icon(Icons.add),
+                       ),
+                       isRequired: true,
+                       bloc: context.read<ServicesBloc>(),
+                       fetchAllFunction: (bloc) => bloc.add(LoadServicesEvent()),
+                       searchFunction: (bloc, query) => bloc.add(LoadServicesEvent(search: query)),
+                       validator: (value) {
+                         if (value.isEmpty) {
+                           return tr.required(tr.individuals);
+                         }
+                         return null;
+                       },
+                       itemBuilder: (context, ser) => Padding(
+                         padding: const EdgeInsets.symmetric(
+                           horizontal: 5,
+                           vertical: 8,
+                         ),
+                         child: Column(
+                           crossAxisAlignment: CrossAxisAlignment.start,
+                           children: [
+                             Row(
+                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                               children: [
+                                 Expanded(
+                                   child: Text(
+                                     "${ser.srvName}",
+                                     style: Theme.of(context).textTheme.bodyLarge,
+                                   ),
+                                 ),
+                               ],
+                             ),
+                           ],
+                         ),
+                       ),
+                       itemToString: (ser) => "${ser.srvName}",
+                       stateToLoading: (state) => state is ServicesLoadingState,
+                       loadingBuilder: (context) => const SizedBox(
+                         width: 16,
+                         height: 16,
+                         child: CircularProgressIndicator(strokeWidth: 2),
+                       ),
+                       stateToItems: (state) {
+                         if (state is ServicesLoadedState) {
+                           return state.services;
+                         }
+                         return [];
+                       },
+                       onSelected: (value) {
+                         setState(() {
+                          serviceId = value.srvId;
+                         });
+                       },
+                       noResultsText: tr.noDataFound,
+                       showClearButton: true,
+                     ),
+                   ),
                    Expanded(
                      child: ZTextFieldEntitled(
+                         isRequired: true,
                          validator: (value){
                            if(value.isEmpty){
                              return tr.required(tr.qty);
@@ -183,6 +194,7 @@ class _DesktopState extends State<_Desktop> {
                    ),
                    Expanded(
                      child: ZTextFieldEntitled(
+                         isRequired: true,
                          validator: (value){
                            if(value.isEmpty){
                              return tr.required(tr.qty);
@@ -193,11 +205,31 @@ class _DesktopState extends State<_Desktop> {
                          title: tr.amount),
                    ),
                  ],
-               )
+               ),
 
+               SizedBox(height: 12),
+               Row(
+                 children: [
+                   ZOutlineButton(
+                       onPressed: (){
+                         Navigator.of(context).pop();
+                       },
+                       label: Text(tr.cancel.toUpperCase())),
+                   SizedBox(width: 8),
+
+                   ZOutlineButton(
+                       onPressed: (){
+
+                       },
+                       isActive: true,
+                       label: Text(tr.create.toUpperCase())),
+
+                 ],
+               )
               ],
             ),
           ),
+          SizedBox(height: 8),
           Container(
             padding: EdgeInsets.symmetric(horizontal: 8,vertical: 7),
             decoration: BoxDecoration(
@@ -299,5 +331,27 @@ class _DesktopState extends State<_Desktop> {
         ],
       ),
     );
+  }
+
+  void onServicesSubmit(){
+    if (!formKey.currentState!.validate()) return;
+    final bloc = context.read<ProjectServicesBloc>();
+
+    final data = ProjectServicesModel(
+      prjId: widget.projectId, // Project ID
+      pjdId: widget.projectId, // Project Services ID, the one supposed to be updated when it's clicked
+      srvId: serviceId, // Service Id
+      srvName: servicesController.text,
+      pjdQuantity: double.tryParse(qty.text),
+      pjdPricePerQty: double.tryParse(amount.text),
+      usrName: loginData?.usrName
+    );
+
+    //To Add A new Service
+    bloc.add(AddProjectServiceEvent(data));
+
+    //To Update selected Service
+    bloc.add(UpdateProjectServiceEvent(data));
+
   }
 }
