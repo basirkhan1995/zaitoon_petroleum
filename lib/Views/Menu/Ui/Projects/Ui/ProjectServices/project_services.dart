bc1e69b0f0056d679a8b93a817496d5c8568d879
HP <basirkhan.hashemi@gmail.com>
2026-02-24 23:51:38 +0430

    invoice commit

--- lib/Views/Menu/Ui/Projects/Ui/ProjectServices/project_services.dart
+++ lib/Views/Menu/Ui/Projects/Ui/ProjectServices/project_services.dart
@@ -55,8 +55,8 @@ class _DesktopState extends State<_Desktop> {
   String? myLocale;
   @override
   void initState() {
+    myLocale = context.read<LocalizationBloc>().state.languageCode;
     WidgetsBinding.instance.addPostFrameCallback((_){
-      myLocale = context.read<LocalizationBloc>().state.languageCode;
       if(widget.projectId !=null) {
         context.read<ProjectServicesBloc>().add(LoadProjectServiceEvent(widget.projectId!));
       }
@@ -78,69 +78,102 @@ class _DesktopState extends State<_Desktop> {
     final tr = AppLocalizations.of(context)!;
     final color = Theme.of(context).colorScheme;
     TextStyle? titleStyle = Theme.of(context).textTheme.titleSmall;
-    TextStyle? subtitleStyle = Theme.of(context).textTheme.titleSmall?.copyWith(color: color.outline.withValues(alpha: .5));
+    TextStyle? subtitleStyle = Theme.of(context).textTheme.titleSmall?.copyWith(color: color.outline.withValues(alpha: .8));
     return Scaffold(
-      body: BlocBuilder<ProjectServicesBloc, ProjectServicesState>(
-        builder: (context, state) {
-          if(state is ProjectServicesLoadingState){
-            return Center(child: CircularProgressIndicator());
-          }
-          if(state is ProjectServicesErrorState){
-            return NoDataWidget(
-              title: tr.errorTitle,
-              message: state.message,
-              onRefresh: (){
-                context.read<ProjectServicesBloc>().add(LoadProjectServiceEvent(widget.projectId!));
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 8,vertical: 7),
+            decoration: BoxDecoration(
+                color: color.primary.withValues(alpha: .05)
+            ),
+            child: Row(
+              children: [
+                Expanded(child: Text("Project Services",style: titleStyle)),
+
+                SizedBox(
+                    width: 50,
+                    child: Text(
+                        tr.qty,style: titleStyle)),
+                SizedBox(
+                    width: 120,
+                    child: Text(
+                        textAlign: myLocale == "en"? TextAlign.right : TextAlign.left,
+                        tr.amount,style: titleStyle)),
+
+                SizedBox(
+                    width: 120,
+                    child: Text(
+                        textAlign: myLocale == "en"? TextAlign.right : TextAlign.left,
+                        tr.totalTitle,style: titleStyle)),
+              ],
+            ),
+          ),
+          Expanded(
+            child: BlocBuilder<ProjectServicesBloc, ProjectServicesState>(
+              builder: (context, state) {
+                if(state is ProjectServicesLoadingState){
+                  return Center(child: CircularProgressIndicator());
+                }
+                if(state is ProjectServicesErrorState){
+                  return NoDataWidget(
+                    title: tr.errorTitle,
+                    message: state.message,
+                    onRefresh: (){
+                      context.read<ProjectServicesBloc>().add(LoadProjectServiceEvent(widget.projectId!));
+                    },
+                  );
+                }
+                if(state is ProjectServicesLoadedState){
+                  return ListView.builder(
+                      itemCount: state.projectServices.length,
+                      itemBuilder: (context,index){
+                      final prjServices = state.projectServices[index];
+                      return Container(
+                        padding: EdgeInsets.symmetric(horizontal: 8,vertical: 8),
+                        decoration: BoxDecoration(
+                          color: index.isOdd? color.primary.withValues(alpha: .05) : Colors.transparent
+                        ),
+                        child: Row(
+                          children: [
+                           Expanded(
+                             child: Column(
+                               mainAxisAlignment: MainAxisAlignment.start,
+                               crossAxisAlignment: CrossAxisAlignment.start,
+                               children: [
+                                 Text(prjServices.srvName??"",style: titleStyle),
+                                 Text(prjServices.prpTrnRef??"",style: subtitleStyle),
+                                 if(prjServices.pjdRemark !=null)
+                                 Text(prjServices.pjdRemark??"",style: subtitleStyle),
+                               ],
+                             ),
+                           ),
+
+                           SizedBox(
+                               width: 50,
+                               child: Text(prjServices.pjdQuantity.toString())),
+
+                            SizedBox(
+                                width: 120,
+                                child: Text(prjServices.pjdPricePerQty.toAmount(),
+                                textAlign: myLocale == "en"? TextAlign.right : TextAlign.left,
+                                )),
+
+                            SizedBox(
+                                width: 120,
+                                child: Text(prjServices.total.toAmount(),
+                                  textAlign: myLocale == "en"? TextAlign.right : TextAlign.left,
+                                )),
+                          ],
+                        ),
+                      );
+                  });
+                }
+                return const SizedBox();
               },
-            );
-          }
-          if(state is ProjectServicesLoadedState){
-            return ListView.builder(
-                itemCount: state.projectServices.length,
-                itemBuilder: (context,index){
-                final prjServices = state.projectServices[index];
-                return Container(
-                  padding: EdgeInsets.symmetric(horizontal: 8,vertical: 7),
-                  decoration: BoxDecoration(
-                    color: index.isOdd? color.primary.withValues(alpha: .05) : Colors.transparent
-                  ),
-                  child: Row(
-                    children: [
-                     Expanded(
-                       child: Column(
-                         mainAxisAlignment: MainAxisAlignment.start,
-                         crossAxisAlignment: CrossAxisAlignment.start,
-                         children: [
-                           Text(prjServices.srvName??"",style: titleStyle),
-                           Text(prjServices.prpTrnRef??"",style: subtitleStyle),
-                           if(prjServices.pjdRemark !=null)
-                           Text(prjServices.pjdRemark??"",style: subtitleStyle),
-                         ],
-                       ),
-                     ),
-
-                     SizedBox(
-                         width: 50,
-                         child: Text(prjServices.pjdQuantity.toString())),
-
-                      SizedBox(
-                          width: 100,
-                          child: Text(prjServices.pjdPricePerQty.toAmount(),
-                          textAlign: myLocale == "en"? TextAlign.right : TextAlign.left,
-                          )),
-
-                      SizedBox(
-                          width: 100,
-                          child: Text(prjServices.total.toAmount(),
-                            textAlign: myLocale == "en"? TextAlign.right : TextAlign.left,
-                          )),
-                    ],
-                  ),
-                );
-            });
-          }
-          return const SizedBox();
-        },
+            ),
+          ),
+        ],
       ),
     );
   }
